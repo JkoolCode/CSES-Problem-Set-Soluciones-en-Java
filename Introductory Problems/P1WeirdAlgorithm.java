377457169416c1d233225f4baf5b490ba511d670
Junior Oblitas <cjr.oblitas@gmail.com>
2021-02-25 16:03:12 -0500

    Soluciones en Java

---
+++ Introductory Problems/P1WeirdAlgorithm.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @author Junior Oblitas
+ * Link Problema: https://cses.fi/problemset/task/1068
+ * Link Video Solución: 
+ */
+public class P1WeirdAlgorithm {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        
+        long n = in.nextInt();
+        out.print(n + " ");
+        
+        while(n > 1){
+            if(n % 2 == 0)
+                n /= 2;
+            else
+                n = n * 3 + 1;    
+            out.print(n + " ");        
+        }
+        
+        out.close();
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
