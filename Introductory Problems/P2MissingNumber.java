cdd6fe2e6e0c7f4e05c33d4bcf45248bb0eac941
Junior Oblitas <cjr.oblitas@gmail.com>
2021-02-26 16:49:28 -0500

    Solución

---
+++ Introductory Problems/P2MissingNumber.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @author Junior Oblitas
+ * Link Problema: https://cses.fi/problemset/task/1083
+ * Link Video Solución: 
+ */
+public class P2MissingNumber {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        solve(1, in, out);
+        out.close();
+    }
+
+    static void solve(int testCases, InputReader in, PrintWriter out){
+        int n, x;
+        long sum = 0, ope = 0;
+        
+        n = in.nextInt();
+        for(int i=0; i < n-1; i++){
+            x = in.nextInt();
+            sum += x;
+        }
+        ope = n * (n + 1) / 2;
+        out.println(ope - sum);
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
