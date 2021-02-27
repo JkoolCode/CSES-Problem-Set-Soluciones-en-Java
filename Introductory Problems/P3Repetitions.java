cc25a5e1671e0e6f7f198f2f7a7c99074fc3afc6
Junior Oblitas <cjr.oblitas@gmail.com>
2021-02-27 13:22:17 -0500

    Java

---
+++ Introductory Problems/P3Repetitions.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @author Junior Oblitas
+ * Link Problema: https://cses.fi/problemset/task/1069
+ * Link Video Solución: 
+ */
+public class P3Repetitions {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        solve(1, in, out);
+        out.close();
+    }
+
+    static void solve(int testCases, InputReader in, PrintWriter out){
+        String cadena;
+        long valorMax = 1, cont = 1;
+        
+        cadena = in.next();
+        for(int i=0; i < cadena.length()-1; i++){
+            if(cadena.charAt(i) == cadena.charAt(i+1)){
+                cont++;
+                valorMax = Math.max(cont, valorMax);
+            }else{
+                cont = 1;
+            }
+        }
+        out.println(valorMax); 
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
