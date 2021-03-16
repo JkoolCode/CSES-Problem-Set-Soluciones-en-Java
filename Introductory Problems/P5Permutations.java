cca1df4feacf69bed5aaa36aee102e579d09d62a
Junior Oblitas <cjr.oblitas@gmail.com>
2021-03-16 12:09:15 -0500

    cool

---
+++ Introductory Problems/P5Permutations.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @author Junior Oblitas
+ * Link Problema: https://cses.fi/problemset/task/1070
+ * Link Video Solución: 
+ */
+public class P5Permutations {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        solve(1, in, out);
+        out.close();
+    }
+
+    static void solve(int testCases, InputReader in, PrintWriter out){
+        long n = in.nextLong();
+        if(n == 1){ out.println("1"); return; }
+        if(n > 3){
+            for (long i = n-1; 1 <= i; i -= 2) out.print(i +" ");
+            for (long i = n; 1 <= i; i -= 2) out.print(i +" ");
+        }else{
+            out.println("NO SOLUTION");
+        }
+    }
+
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+ 
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
